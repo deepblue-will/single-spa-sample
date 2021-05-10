7959735c23446b63199e3fa42a1b93c3615f5328
deepblue-will <deepblue.will@gmail.com>
2021-05-10 09:33:42 +0900

    glonav-app

--- apps/global-nav/src/app/app.module.ts
+++ apps/global-nav/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NzLayoutModule, NzMenuModule],
   providers: [],
   bootstrap: [AppComponent],
 })
