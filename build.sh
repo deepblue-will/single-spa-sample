89b30a062b8db3f869ea7e3e83a710ab8e01ce63
deepblue-will <deepblue.will@gmail.com>
2021-05-10 16:49:15 +0900

    deploy to cloudflare page

---
+++ build.sh
@@ -0,0 +1,24 @@
+#bin/bash -ex
+
+echo "install and build root"
+
+npm i
+npm run build
+
+echo "install and build global-nav"
+
+cd apps/global-nav
+npm i
+npm run build:single-spa:global-nav
+cp -r ./dist/global-nav ../../dist/global-nav
+cd ..
+
+echo "install and build angular-app"
+
+cd apps/angular-app
+npm i
+npm run build:single-spa:angular-app
+cp -r ./dist/angular-app ../../dist/angular-app
+cd ..
+
+echo "end"
