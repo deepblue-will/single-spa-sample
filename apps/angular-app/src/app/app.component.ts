fbdd095b9a57aa02abe7cd7c9398865bce88d9bf
deepblue-will <deepblue.will@gmail.com>
2021-05-10 14:52:01 +0900

    styled

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { NzIconService } from 'ng-zorro-antd/icon';
+import { assetUrl } from '../single-spa/asset-url';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(iconService: NzIconService) {
+    iconService.changeAssetsSource(assetUrl('../'));
+  }
+}
--- apps/angular-app/src/app/app.module.ts
+++ apps/angular-app/src/app/app.module.ts
@@ -6,10 +6,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { MainComponent } from './main/main.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { HttpClientModule } from '@angular/common/http';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 @NgModule({
   declarations: [AppComponent, EmptyRouteComponent, MainComponent],
-  imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule],
+  imports: [
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
