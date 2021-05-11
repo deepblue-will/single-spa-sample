4e3e657ff126b98658121d08cb75661c83bba07f
deepblue-will <deepblue.will@gmail.com>
2021-05-11 09:31:31 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#bin/bash -ex
+#!/bin/bash -ex
 
 echo "install and build root"
 
@@ -11,7 +11,7 @@ cd apps/global-nav
 npm i
 npm run build:single-spa:global-nav
 cp -r ./dist/global-nav ../../dist/global-nav
-cd ..
+cd ../../
 
 echo "install and build angular-app"
 
@@ -19,6 +19,6 @@ cd apps/angular-app
 npm i
 npm run build:single-spa:angular-app
 cp -r ./dist/angular-app ../../dist/angular-app
-cd ..
+cd ../../
 
 echo "end"
